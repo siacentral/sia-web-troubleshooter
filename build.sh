da232dbe438a7dea2454b84c55759c9d9f2bba49
Nate Maninger <me@n8m.us>
2020-05-07 13:56:40 +0900

    update build

--- build.sh
+++ build.sh
@@ -1,3 +1,2 @@
-docker build -t siacentral/web/troubleshooter .
-docker tag siacentral/web/troubleshooter:latest 921261815063.dkr.ecr.us-east-2.amazonaws.com/siacentral/web/troubleshooter:latest
-docker push 921261815063.dkr.ecr.us-east-2.amazonaws.com/siacentral/web/troubleshooter:latest
+docker build -t registry.gitlab.com/siacentral/web/troubleshooter:latest .
+docker push registry.gitlab.com/siacentral/web/troubleshooter:latest
