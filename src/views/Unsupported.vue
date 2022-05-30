2845ca306f15bd8c6f2032205ad897a7aee7ef29
Nate Maninger <me@n8m.us>
2022-05-29 22:37:26 -0600

    remove scp

--- src/views/Unsupported.vue
+++ src/views/Unsupported.vue
@@ -1,3 +1,23 @@
 <template>
-	<h1>Error - not found</h1>
+	<div class="error-alert">
+		<h3>Error - not found</h3>
+		<router-link to="/" class="btn btn-success btn-inline">Back</router-link>
+	</div>
 </template>
+
+<style lang="stylus" scoped>
+	.error-alert {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+		height: 100%;
+		width: 100%;
+		font-size: 1.5em;
+		text-align: center;
+	}
+
+	a {
+		text-decoration: none;
+	}
+</style>
